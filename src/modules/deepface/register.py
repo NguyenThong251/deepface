bc6d5ac1d1d095450c4355e081f440a785bb78c4
NguyenThong251 <ht01252004@gmail.com>
2025-10-16 23:43:09 +0700

    add qdrant database

--- src/modules/deepface/register.py
+++ src/modules/deepface/register.py
@@ -3,21 +3,42 @@ import numpy as np
 
 from src.services.sql import SQLService
 from src.services.redis import RedisService
+from src.services.qdrant import QdrantService
+from src.services.detect_face import DetectFaceService
+from src.utils.imgbase64 import decode_base64_image
 
 class RegisterController:
     def __init__(self):
         self.sql_service = SQLService()
         self.redis_service = RedisService()
+        self.qdrant_service = QdrantService()
+        self.face_detect_service = DetectFaceService()
     
     def register_user(self, data: Dict[str, Any]):
         try:
             user_id = data.get("user_id")
+            # test
+            img_test = data.get("image_test")
             if not user_id: return {'success': False,"error": {'message':"VALIDATION FAILED"}}
 
             if self.sql_service.face_user_exists(user_id): 
                 return {'success': False,"error": {'message':"FACE USER EXISTS"}}
 
             image_face = self.redis_service.get_temp_image(user_id)
+            
+
+            # vector database
+            imagergb_face = decode_base64_image(img_test)
+            faces = self.face_detect_service.detect_face(imagergb_face)
+            if isinstance(faces, dict) and faces.get('success') is False:
+                return faces
+            face = faces[0]
+            x, y, w, h = int(face.x), int(face.y), int(face.w), int(face.h)
+            face_crop = imagergb_face[y:y+h, x:x+w]
+
+            face_vector = self.qdrant_service.insert_vector(collection_name="faces",point_id=user_id,vector=face_crop,payload={"user_id": user_id})
+            # vector database
+
             if image_face is None: return {'success': False,"error": {'message':"FACE NOT FOUND"}}
 
             res_sql = self.sql_service.create_face_info(user_id , image_face)
@@ -28,4 +49,4 @@ class RegisterController:
             return {'success': True, 'result': {'message': 'OK'}}
             
         except Exception as e:
-            return {'success': False,"error": {'message': 'SYSTEM ERROR'}}
+            return {'success': False,"error": {'message': 'SYSTEM ERROR' + str(e)}}
--- src/utils/imgbase64.py
+++ src/utils/imgbase64.py
@@ -1,5 +1,5 @@
 import base64, io, numpy as np
 from PIL import Image
 
-def decode_base64_image(b64: str) -> np.ndarray:
+def decode_base64_image(b64) -> np.ndarray:
     return np.array(Image.open(io.BytesIO(base64.b64decode(b64.split(",",1)[-1]))).convert("RGB"))[:, :, ::-1]
