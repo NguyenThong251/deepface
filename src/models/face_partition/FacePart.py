1eea9ed1d0cfe9cb124c49357a1f6965d678d9ec
Nguyễn Hoàng Thông <125713226+NguyenThong251@users.noreply.github.com>
2025-11-10 11:15:09 +0700

    fix detect partitions

--- src/models/face_partition/FacePart.py
+++ src/models/face_partition/FacePart.py
@@ -7,7 +7,7 @@ class FacePartition:
         self.model_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "yolov8n.pt")
         self.model = YOLO(self.model_path)
     
-    def has_mouth(self, img: np.ndarray, conf_thres: float = 0.5):
+    def has_mouth(self, img: np.ndarray):
         res = self.model(img, agnostic_nms=True, verbose=False)[0]
         boxes = getattr(res, "boxes", None)
 
@@ -16,10 +16,6 @@ class FacePartition:
 
         labels = []
         for cls_tensor, conf_tensor in zip(boxes.cls, boxes.conf):
-            conf = float(conf_tensor)
-            if conf < conf_thres:
-                continue
-
             cls_id = int(cls_tensor)
             label = self.model.names[cls_id].lower()
             labels.append(label)
@@ -28,8 +24,7 @@ class FacePartition:
         mouth_count = labels.count("mouth")
         eye_count = labels.count("eye")
         eyebrow_count = labels.count("eyebrow")
-        
-        if nose_count >= 1 and mouth_count >= 1 and eye_count >= 2 and eyebrow_count >= 2:
+        if nose_count >= 1 and mouth_count >= 1 and eye_count >= 2 and eyebrow_count >= 2 :
             return False
         else:
             return True
